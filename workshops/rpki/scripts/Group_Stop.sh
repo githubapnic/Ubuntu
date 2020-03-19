292e3924810aceca0d858662b92958eedeb5e4ec
githubapnic <56416066+githubapnic@users.noreply.github.com>
2020-03-19 23:56:45 +1000

    Create Group_Stop.sh

---
+++ workshops/rpki/scripts/Group_Stop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Purpose: Use this to stop all the groupXX containers
+# Usage: ./Group_Stop.sh 
+
+for i in {1..30}
+do 
+	if test $i -lt 10; then
+		echo STOP container group0$i.apnictraining.net 
+		sudo lxc-stop -n group0$i.apnictraining.net 
+	else
+		echo STOP container group$i.apnictraining.net 
+		sudo lxc-stop -n group$i.apnictraining.net 
+	fi
+	sleep 2
+done
+
+sudo lxc-ls --fancy 
