548908c7020ad9c5d91b1f4798697b9570bfc28a
githubapnic <56416066+githubapnic@users.noreply.github.com>
2020-03-21 23:50:06 +1000

    Add files via upload

--- workshops/rpki/setup_rpki_workshop.sh
+++ workshops/rpki/setup_rpki_workshop.sh
@@ -87,13 +87,11 @@ function createLXCtemplate()
     sudo cp -p /usr/share/lxc/templates/lxc-ubuntu /usr/share/lxc/templates/lxc-ubuntu-apnic >> $LOG_FILE
 	sudo sed -i 's/apt-transport-https,ssh,vim/nano/' /usr/share/lxc/templates/lxc-ubuntu-apnic  >> $LOG_FILE
 	#sudo sed -i 's/packages_template\=\"\${packages_t/\#packages_template\=\"\${packages_t/' /usr/share/lxc/templates/lxc-ubuntu-apnic >> $LOG_FILE
-    # sudo sed -i 's/user=\$4/user=apnic/' /usr/share/lxc/templates/lxc-ubuntu-apnic >> $LOG_FILE
-    # sudo sed -i 's/user=\"ubuntu\"/user=apnic/' /usr/share/lxc/templates/lxc-ubuntu-apnic >> $LOG_FILE
-    # sudo sed -i 's/password=\"ubuntu\"/password=training/' /usr/share/lxc/templates/lxc-ubuntu-apnic >> $LOG_FILE
     # Download and create a container
     #sudo lxc-create -n template.apnictraining.net -t ubuntu-apnic >> $LOG_FILE
 	#sudo lxc-create -n template.apnictraining.net -t ubuntu-apnic -- --user $user --password $password --packages openssh-server,gcc,curl,build-essential >> $LOG_FILE
-	sudo lxc-create -n template.apnictraining.net -t ubuntu-apnic -- --user $user --password $password --packages $TEMPLATE_PACKAGES --variant minbase >> $LOG_FILE
+	#sudo lxc-create -n template.apnictraining.net -t ubuntu-apnic -- --user $user --password $password --packages $TEMPLATE_PACKAGES --variant minbase >> $LOG_FILE
+	sudo lxc-create -n template.apnictraining.net -t ubuntu-apnic -- --user $user --password $password --packages $TEMPLATE_PACKAGES >> $LOG_FILE
     echo "###### Update IP details to 192.168.30.100" | tee -a $LOG_FILE
     # Update the IP address
     sudo cp 10-lxc.yaml /var/lib/lxc/template.apnictraining.net/rootfs/etc/netplan/10-lxc.yaml >> $LOG_FILE
