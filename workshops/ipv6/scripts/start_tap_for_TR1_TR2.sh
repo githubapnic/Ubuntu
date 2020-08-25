6e531424ec7e35bd30981150409ffbe54a241c6b
githubapnic <56416066+githubapnic@users.noreply.github.com>
2020-08-25 13:24:44 +1000

    Add files via upload

--- workshops/ipv6/scripts/start_tap_for_TR1_TR2.sh
+++ workshops/ipv6/scripts/start_tap_for_TR1_TR2.sh
@@ -5,17 +5,19 @@
 # Usage: ./start_tap_for_TR1_TR2.sh [bridgename]
 #			./start_tap_for_TR1_TR2.sh LAN
 
-BRIDGENAME=%1
+BRIDGENAME=$1
 
-if [[ BRIDGENAME =="" ]]; then
+# https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty
+# Check if Bridgename exists
+if [[ -z $BRIDGENAME ]]; then
   BRIDGENAME="LAN"
 fi
 
 for i in {1..2}
 do 
-  sudo ifconfig TR$i up
-  sudo brctl addif $BRIDGENAME TR$i
+  sudo ifconfig tap-lan$i up
+  sudo brctl addif $BRIDGENAME tap-lan$i
   sleep 1
 done
-ifconfig | grep TR
+ifconfig | grep tap
 brctl show
