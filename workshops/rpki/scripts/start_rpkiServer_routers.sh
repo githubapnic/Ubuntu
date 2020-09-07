4d5c658a5caf5115bc4a90c6fc45dd4d0f0d930b
githubapnic <56416066+githubapnic@users.noreply.github.com>
2020-09-07 16:02:17 +1000

    Add files via upload

--- workshops/rpki/scripts/start_rpkiServer_routers.sh
+++ workshops/rpki/scripts/start_rpkiServer_routers.sh
@@ -9,4 +9,8 @@ sleep 2
 cd $HOME/virtual_labs/rpki/
 sudo ./run-dynamips
 sleep 1
-sudo dynagen topology.net
+screen -dmS dynagenSession
+screen -S dynagenSession -p 0 -X stuff 'dynagen /home/apnic/virtual_labs/rpki/topology.net\r'
+sleep 1
+cd $HOME/Documents/scripts
+sudo ./start_rpki_tap.sh
